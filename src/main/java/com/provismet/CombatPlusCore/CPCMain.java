3309f71a0651df7c618fa0b1bb2311334a2c3770
Provismet <17149901+Provismet@users.noreply.github.com>
2023-12-23 04:23:09 +0000

    Add identifier method.

--- src/main/java/com/provismet/CombatPlusCore/CPCMain.java
+++ src/main/java/com/provismet/CombatPlusCore/CPCMain.java
@@ -4,10 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class CPCMain implements ModInitializer {
-    public static final String MODID = "combatplus-core";
+    public static final String MODID = "combat-plus";
     public static final Logger LOGGER = LoggerFactory.getLogger("Combat+ Core");
+
+    public static Identifier identifier (String path) {
+        return Identifier.of(MODID, path);
+    }
 
     @Override
     public void onInitialize () {
