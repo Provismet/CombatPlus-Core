774a654c2e5ca6258b0ae37c98950efd9a207166
Provismet <17149901+Provismet@users.noreply.github.com>
2023-12-29 04:28:32 +0000

    Add attribute UUIDs.

---
+++ src/main/java/com/provismet/CombatPlusCore/utility/AttributeIdentifiers.java
@@ -0,0 +1,7 @@
+package com.provismet.CombatPlusCore.utility;
+
+import java.util.UUID;
+
+public class AttributeIdentifiers {
+    public static final UUID OFFHAND_DAMAGE = UUID.nameUUIDFromBytes("Combat+ Offhand Weapon Damage".getBytes());
+}
