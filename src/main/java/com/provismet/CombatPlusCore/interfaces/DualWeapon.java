ec71d9f1389d4965678dffffbbd3042d33cf40e9
Provismet <17149901+Provismet@users.noreply.github.com>
2024-02-03 21:53:43 +0000

    Make damage data types consistent.

--- src/main/java/com/provismet/CombatPlusCore/interfaces/DualWeapon.java
+++ src/main/java/com/provismet/CombatPlusCore/interfaces/DualWeapon.java
@@ -3,15 +3,15 @@ package com.provismet.CombatPlusCore.interfaces;
 import com.provismet.CombatPlusCore.enchantments.OffHandEnchantment;
 import com.provismet.lilylib.util.MoreMath;
 
-import net.minecraft.util.math.MathHelper;
-
 /**
  * Interface for melee weapons that can be held in either hand.
  * 
  * <p> Dual weapons are applicable for {@link OffHandEnchantment} by default.
  */
 public interface DualWeapon extends MeleeWeapon {
-    public default double getOffhandDamage () {
-        return MathHelper.absMax(0.5, MoreMath.roundDownToMultiple(this.getWeaponDamage() / 3.5, 0.5));
+    public default float getOffhandDamage () {
+        float defaultValue = (float)MoreMath.roundDownToMultiple(this.getWeaponDamage() / 3.5f, 0.5);
+        if (defaultValue > 0.5f) return defaultValue;
+        return 0.5f;
     }
 }
