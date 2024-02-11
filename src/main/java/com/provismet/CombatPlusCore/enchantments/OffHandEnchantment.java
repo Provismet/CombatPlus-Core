6734137c1beb6534e26912476b58fda2592fdb78
Provismet <17149901+Provismet@users.noreply.github.com>
2024-02-11 05:03:10 +0000

    Add grouping system to offhand enchantments.

--- src/main/java/com/provismet/CombatPlusCore/enchantments/OffHandEnchantment.java
+++ src/main/java/com/provismet/CombatPlusCore/enchantments/OffHandEnchantment.java
@@ -1,5 +1,7 @@
 package com.provismet.CombatPlusCore.enchantments;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.provismet.CombatPlusCore.interfaces.CPCEnchantment;
 import com.provismet.CombatPlusCore.utility.CPCEnchantmentHelper;
 import com.provismet.CombatPlusCore.utility.CPCEnchantmentTargets;
@@ -30,6 +32,24 @@ public abstract class OffHandEnchantment extends Enchantment implements CPCEncha
             !(other instanceof SweepingEnchantment) &&
             !(other instanceof LuckEnchantment) &&
             !CPCEnchantmentHelper.isDamage(other) &&
-            !CPCEnchantmentHelper.isAdditionalDamage(other);
+            !CPCEnchantmentHelper.isAdditionalDamage(other) &&
+            !(other instanceof OffHandEnchantment otherOffhand &&
+                otherOffhand.getGroup() != null &&
+                this.getGroup() != null &&
+                otherOffhand.getGroup() != this.getGroup()
+            );
     }
+
+    /**
+     * Returns the group name of this enchantment. Offhand enchantments that have a group cannot
+     * be mixed with offhand enchantments that belong to a different group.
+     * 
+     * <p> This system exists because offhand enchantments may be designed to interact with each other (see the Dual Swords mod).
+     * The Offhand Enchantment class is intentionally miscellaneous by nature, so this library will offer a rudimentary grouping
+     * system instead of direct subclasses.
+     * 
+     * @return A unique string denoting the group name of this enchantment, or null.
+     */
+    @Nullable
+    protected abstract String getGroup ();
 }
