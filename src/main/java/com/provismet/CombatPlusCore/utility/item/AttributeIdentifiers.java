a18f663e67dc0d22c458a751f0bc109bdd71c0a1
Provismet <17149901+Provismet@users.noreply.github.com>
2024-06-22 00:55:43 +0100

    Attribute IDs are now identifiers, not UUIDs.

--- src/main/java/com/provismet/CombatPlusCore/utility/item/AttributeIdentifiers.java
+++ src/main/java/com/provismet/CombatPlusCore/utility/item/AttributeIdentifiers.java
@@ -1,8 +1,9 @@
 package com.provismet.CombatPlusCore.utility.item;
 
-import java.util.UUID;
+import com.provismet.CombatPlusCore.CPCMain;
+import net.minecraft.util.Identifier;
 
 public class AttributeIdentifiers {
-    public static final UUID OFFHAND_DAMAGE = UUID.nameUUIDFromBytes("Combat+ Offhand Weapon Damage".getBytes());
-    public static final UUID WEAPON_BONUS_ATTRIBUTE = UUID.nameUUIDFromBytes("Combat+ Weapon Bonus Attribute".getBytes());
+    public static final Identifier OFFHAND_DAMAGE = CPCMain.identifier("offhand_damage");
+    public static final Identifier WEAPON_BONUS_ATTRIBUTE = CPCMain.identifier("weapon_bonus_attribute");
 }
