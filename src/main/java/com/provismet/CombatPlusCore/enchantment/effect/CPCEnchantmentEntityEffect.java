6468b1c1d34fdc87805a53fe417f53841dd36ee3
Provismet <17149901+Provismet@users.noreply.github.com>
2024-06-24 23:15:47 -0700

    Fix import.

--- src/main/java/com/provismet/CombatPlusCore/enchantment/effect/CPCEnchantmentEntityEffect.java
+++ src/main/java/com/provismet/CombatPlusCore/enchantment/effect/CPCEnchantmentEntityEffect.java
@@ -2,7 +2,6 @@ package com.provismet.CombatPlusCore.enchantment.effect;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
-import com.provismet.CombatPlusCore.registries.CPCEnchantmentDoubleEntityEffects;
 import com.provismet.CombatPlusCore.utility.CPCRegistries;
 import net.minecraft.enchantment.EnchantmentEffectContext;
 import net.minecraft.entity.Entity;
@@ -13,7 +12,7 @@ import java.util.function.Function;
 /**
  * Combat+ Enchantment Effect for effects that can look at both the attacker and the target simultaneously.
  *
- * @see CPCEnchantmentDoubleEntityEffects
+ * @see com.provismet.CombatPlusCore.registries.DoubleEntityEffects
  */
 public interface CPCEnchantmentEntityEffect {
     public static final Codec<CPCEnchantmentEntityEffect> CODEC = CPCRegistries.ENCHANTMENT_DUAL_ENTITY_EFFECT_TYPE.getCodec().dispatch(CPCEnchantmentEntityEffect::getCodec, Function.identity());
