6fd575d9990dd8aab64eb703102f75c6600da7b7
Provismet <17149901+Provismet@users.noreply.github.com>
2024-06-29 00:30:20 -0700

    Override vanilla enchantments.
    This mirrors the 1.20.6 functionality where supported item tags are overridden.

--- src/main/java/com/provismet/datagen/CombatPlusCore/EnchantmentGenerator.java
+++ src/main/java/com/provismet/datagen/CombatPlusCore/EnchantmentGenerator.java
@@ -2,18 +2,39 @@ package com.provismet.datagen.CombatPlusCore;
 
 import com.provismet.CombatPlusCore.CPCMain;
 import com.provismet.CombatPlusCore.enchantment.effect.doubleEntity.ApplyToBothEntityEnchantmentEffect;
+import com.provismet.CombatPlusCore.enchantment.effect.singleEntity.CodeExecutionSingleEntityEffect;
 import com.provismet.CombatPlusCore.enchantment.effect.singleEntity.DamageEquipmentEffect;
+import com.provismet.CombatPlusCore.enchantment.loot.condition.singleEntity.ApplyToAttacker;
+import com.provismet.CombatPlusCore.enchantment.loot.condition.singleEntity.RelativeHealthCondition;
+import com.provismet.CombatPlusCore.enchantment.loot.context.CPCLootContext;
 import com.provismet.CombatPlusCore.registries.CPCEnchantmentComponentTypes;
 import com.provismet.CombatPlusCore.enchantment.effect.doubleEntity.CodeExecutionDoubleEntityEnchantmentEffect;
 import com.provismet.CombatPlusCore.utility.resource.DevModeResourceCondition;
 import com.provismet.CombatPlusCore.utility.tag.CPCItemTags;
 import com.provismet.datagen.CombatPlusCore.provider.CPCEnchantmentProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.component.EnchantmentEffectComponentTypes;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentLevelBasedValue;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.enchantment.effect.EnchantmentEffectTarget;
+import net.minecraft.enchantment.effect.entity.ApplyMobEffectEnchantmentEffect;
+import net.minecraft.enchantment.effect.entity.IgniteEnchantmentEffect;
+import net.minecraft.enchantment.effect.value.AddEnchantmentEffect;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.loot.condition.DamageSourcePropertiesLootCondition;
+import net.minecraft.loot.condition.EntityPropertiesLootCondition;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.predicate.entity.DamageSourcePredicate;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.EntityTypePredicate;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.tag.EnchantmentTags;
+import net.minecraft.registry.tag.EntityTypeTags;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -48,6 +69,117 @@ public class EnchantmentGenerator extends CPCEnchantmentProvider {
                 new CodeExecutionDoubleEntityEnchantmentEffect(CPCMain.identifier("log-kill"))
             ),
             new DevModeResourceCondition()
+        );
+
+        builder.add(
+            Enchantments.SHARPNESS,
+            Enchantment.builder(
+                Enchantment.definition(
+                    builder.getItemEntryList(ItemTags.SHARP_WEAPON_ENCHANTABLE),
+                    builder.getItemEntryList(CPCItemTags.DAMAGE_PRIMARY_ENCHANTABLE),
+                    10,
+                    5,
+                    Enchantment.leveledCost(1, 11),
+                    Enchantment.leveledCost(21, 11),
+                    1,
+                    AttributeModifierSlot.MAINHAND
+                )
+            ).exclusiveSet(
+                builder.getEnchantmentEntryList(EnchantmentTags.DAMAGE_EXCLUSIVE_SET)
+            ).addEffect(
+                EnchantmentEffectComponentTypes.DAMAGE,
+                new AddEnchantmentEffect(
+                    EnchantmentLevelBasedValue.linear(1.0f, 0.5f)
+                )
+            )
+        );
+
+        builder.add(
+            Enchantments.SMITE,
+            Enchantment.builder(
+                Enchantment.definition(
+                    builder.getItemEntryList(CPCItemTags.DAMAGE_ENCHANTABLE),
+                    builder.getItemEntryList(CPCItemTags.DAMAGE_PRIMARY_ENCHANTABLE),
+                    5,
+                    5,
+                    Enchantment.leveledCost(5, 8),
+                    Enchantment.leveledCost(25, 8),
+                    2,
+                    AttributeModifierSlot.MAINHAND
+                )
+            ).exclusiveSet(
+                builder.getEnchantmentEntryList(EnchantmentTags.DAMAGE_EXCLUSIVE_SET)
+            ).addEffect(
+                EnchantmentEffectComponentTypes.DAMAGE,
+                new AddEnchantmentEffect(EnchantmentLevelBasedValue.linear(2.5f)),
+                EntityPropertiesLootCondition.builder(
+                    LootContext.EntityTarget.THIS,
+                    EntityPredicate.Builder.create().type(EntityTypePredicate.create(EntityTypeTags.SENSITIVE_TO_SMITE))
+                )
+            )
+        );
+
+        builder.add(
+            Enchantments.BANE_OF_ARTHROPODS,
+            Enchantment.builder(
+                Enchantment.definition(
+                    builder.getItemEntryList(CPCItemTags.DAMAGE_ENCHANTABLE),
+                    builder.getItemEntryList(CPCItemTags.DAMAGE_PRIMARY_ENCHANTABLE),
+                    5,
+                    5,
+                    Enchantment.leveledCost(5, 8),
+                    Enchantment.leveledCost(25, 8),
+                    2,
+                    AttributeModifierSlot.MAINHAND)
+            ).exclusiveSet(
+                builder.getEnchantmentEntryList(EnchantmentTags.DAMAGE_EXCLUSIVE_SET)
+            ).addEffect(
+                EnchantmentEffectComponentTypes.DAMAGE,
+                new AddEnchantmentEffect(EnchantmentLevelBasedValue.linear(2.5f)),
+                EntityPropertiesLootCondition.builder(
+                    LootContext.EntityTarget.THIS,
+                    EntityPredicate.Builder.create().type(EntityTypePredicate.create(EntityTypeTags.SENSITIVE_TO_BANE_OF_ARTHROPODS))
+                )
+            ).addEffect(
+                EnchantmentEffectComponentTypes.POST_ATTACK,
+                EnchantmentEffectTarget.ATTACKER,
+                EnchantmentEffectTarget.VICTIM,
+                new ApplyMobEffectEnchantmentEffect(
+                    RegistryEntryList.of(StatusEffects.SLOWNESS),
+                    EnchantmentLevelBasedValue.constant(1.5f),
+                    EnchantmentLevelBasedValue.linear(1.5f, 0.5f),
+                    EnchantmentLevelBasedValue.constant(3.0f),
+                    EnchantmentLevelBasedValue.constant(3.0f)
+                ),
+                EntityPropertiesLootCondition.builder(
+                    LootContext.EntityTarget.THIS,
+                    EntityPredicate.Builder.create().type(EntityTypePredicate.create(EntityTypeTags.SENSITIVE_TO_BANE_OF_ARTHROPODS))
+                ).and(
+                    DamageSourcePropertiesLootCondition.builder(DamageSourcePredicate.Builder.create().isDirect(true))
+                )
+            )
+        );
+
+        builder.add(
+            Enchantments.FIRE_ASPECT,
+            Enchantment.builder(
+                Enchantment.definition(
+                    builder.getItemEntryList(CPCItemTags.ASPECT_ENCHANTABLE),
+                    builder.getItemEntryList(CPCItemTags.ASPECT_PRIMARY_ENCHANTABLE),
+                    2,
+                    2,
+                    Enchantment.leveledCost(10, 20),
+                    Enchantment.leveledCost(60, 20),
+                    4,
+                    AttributeModifierSlot.MAINHAND
+                )
+            ).addEffect(
+                EnchantmentEffectComponentTypes.POST_ATTACK,
+                EnchantmentEffectTarget.ATTACKER,
+                EnchantmentEffectTarget.VICTIM,
+                new IgniteEnchantmentEffect(EnchantmentLevelBasedValue.linear(4.0f)),
+                DamageSourcePropertiesLootCondition.builder(DamageSourcePredicate.Builder.create().isDirect(true))
+            )
         );
     }
 }
--- src/main/java/com/provismet/datagen/CombatPlusCore/ItemTagGenerator.java
+++ src/main/java/com/provismet/datagen/CombatPlusCore/ItemTagGenerator.java
@@ -5,6 +5,7 @@ import com.provismet.CombatPlusCore.utility.tag.CPCItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
@@ -66,6 +67,9 @@ public class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
 
         getOrCreateTagBuilder(ItemTags.WEAPON_ENCHANTABLE)
             .addOptionalTag(CPCItemTags.MELEE_WEAPON);
+
+        getOrCreateTagBuilder(ItemTags.SHARP_WEAPON_ENCHANTABLE)
+            .addOptionalTag(CPCItemTags.DAMAGE_ENCHANTABLE);
 
         getOrCreateTagBuilder(ConventionalItemTags.ENCHANTABLES)
             .addOptionalTag(CPCItemTags.DAMAGE_ENCHANTABLE)
