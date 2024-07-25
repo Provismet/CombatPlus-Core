8298ed7d29c08b4bb6d44e087ee586d8907568ae
Provismet <17149901+Provismet@users.noreply.github.com>
2024-07-25 08:18:55 +0100

    Add minimum value to gamerule.

--- src/main/java/com/provismet/CombatPlusCore/utility/CPCGameRules.java
+++ src/main/java/com/provismet/CombatPlusCore/utility/CPCGameRules.java
@@ -15,7 +15,7 @@ public class CPCGameRules {
     public static final CustomGameRuleCategory CATEGORY = new CustomGameRuleCategory(CPCMain.identifier("gamerule_category"), Text.translatable("gamerule.category.combat-plus").formatted(Formatting.BOLD, Formatting.YELLOW));
     public static final GameRules.Key<BooleanRule> SWEEPING_REQUIRES_ENCHANTMENT = GameRuleRegistry.register("sweepingRequiresEnchantment", CATEGORY, GameRuleFactory.createBooleanRule(false));
     public static final GameRules.Key<BooleanRule> LOYALTY_STAYS_IN_HAND = GameRuleRegistry.register("keepLoyaltyTridents", CATEGORY, GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<DoubleRule> PVP_DAMAGE_MODIFIER = GameRuleRegistry.register("pvpEnchantmentModifier", CATEGORY, GameRuleFactory.createDoubleRule(0.5));
+    public static final GameRules.Key<DoubleRule> PVP_DAMAGE_MODIFIER = GameRuleRegistry.register("pvpEnchantmentModifier", CATEGORY, GameRuleFactory.createDoubleRule(0.5, 0));
 
     public static void init () {
         // Loads the class...
