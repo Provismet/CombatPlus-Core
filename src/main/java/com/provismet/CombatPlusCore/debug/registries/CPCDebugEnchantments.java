5231e87aaba40ecd33d4cf47f942e8c2d23c845d
Provismet <17149901+Provismet@users.noreply.github.com>
2024-11-18 21:55:02 +0000

    Update LilyLib.

--- src/main/java/com/provismet/CombatPlusCore/debug/registries/CPCDebugEnchantments.java
+++ src/main/java/com/provismet/CombatPlusCore/debug/registries/CPCDebugEnchantments.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public class CPCDebugEnchantments {
     private static final EnchantmentContainer LOGGER = new EnchantmentContainer(
         CPCMain.identifier("logger"),
-        (itemLookup, enchantmentLookup, damageLookup, blockLookup) -> Enchantment.builder(
+        (itemLookup, enchantmentLookup, damageLookup, blockLookup, entityLookup) -> Enchantment.builder(
             Enchantment.definition(
                 itemLookup.getOrThrow(CPCItemTags.DAMAGE_ENCHANTABLE),
                 1,
