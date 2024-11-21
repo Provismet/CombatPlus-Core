eeaef0b7c690a7fb700b9816499713fe444dcac0
Provismet <17149901+Provismet@users.noreply.github.com>
2024-11-21 22:12:40 +0000

    Remove unnecessary import.

--- src/main/java/com/provismet/CombatPlusCore/mixin/TridentItemMixin.java
+++ src/main/java/com/provismet/CombatPlusCore/mixin/TridentItemMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.sound.SoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.provismet.CombatPlusCore.utility.CPCGameRules;
 
