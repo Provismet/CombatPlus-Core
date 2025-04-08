768640c0723b5aa73fbb0b7f1699ecc1066be882
Provismet <17149901+Provismet@users.noreply.github.com>
2025-04-08 19:37:57 +0100

    Add constants for configs.

---
+++ src/main/java/com/provismet/CombatPlusCore/utility/CPCConfig.java
@@ -0,0 +1,7 @@
+package com.provismet.CombatPlusCore.utility;
+
+public abstract class CPCConfig {
+    public static final String FOLDER = "./config/combat-plus";
+
+    public static final String KEY_OVERRIDE_DATAPACK_LOOT_TABLES = "override_datapack_loot_tables";
+}
