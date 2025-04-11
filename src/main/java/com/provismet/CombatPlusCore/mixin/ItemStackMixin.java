b95de801e190a50b817445eba32afe0abba272c7
Provismet <17149901+Provismet@users.noreply.github.com>
2025-04-11 18:57:48 +0100

    Items apply enchanted data components on creation.

--- src/main/java/com/provismet/CombatPlusCore/mixin/ItemStackMixin.java
+++ src/main/java/com/provismet/CombatPlusCore/mixin/ItemStackMixin.java
@@ -5,14 +5,17 @@ import com.provismet.CombatPlusCore.registries.CPCEnchantmentComponentTypes;
 import com.provismet.CombatPlusCore.utility.CPCEnchantmentHelper;
 import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.MergedComponentMap;
 import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.effect.EnchantmentEffectEntry;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -20,6 +23,18 @@ import java.util.List;
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
     @Shadow public abstract ItemEnchantmentsComponent getEnchantments ();
+
+    @Inject(method = "<init>(Lnet/minecraft/item/ItemConvertible;ILnet/minecraft/component/MergedComponentMap;)V", at = @At("TAIL"))
+    private void loadEnchantmentDataComponents (ItemConvertible item, int count, MergedComponentMap components, CallbackInfo info) {
+        ItemStack thisItem = (ItemStack)(Object)this;
+
+        CPCEnchantmentHelper.forEachEnchantment((enchantment, level) -> {
+            List<EnchantmentEffectEntry<CPCDataComponentEntityEffect<?>>> componentEffects = enchantment.value().getEffect(CPCEnchantmentComponentTypes.DATA_COMPONENT);
+            for (EnchantmentEffectEntry<CPCDataComponentEntityEffect<?>> effect : componentEffects) {
+                effect.effect().apply(thisItem, level);
+            }
+        }, thisItem);
+    }
 
     @Inject(method = "set", at = @At("HEAD"))
     private <T> void applyItemComponentsFromEnchantments (ComponentType<T> type, @Nullable T value, CallbackInfoReturnable<T> cir) {
