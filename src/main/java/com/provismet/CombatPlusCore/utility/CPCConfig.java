fdb38f1a426639ae5716ed68f4053765ca102d1b
Provismet <17149901+Provismet@users.noreply.github.com>
2025-10-11 10:54:11 +0100

    Add resolved path for config directory.

--- src/main/java/com/provismet/CombatPlusCore/utility/CPCConfig.java
+++ src/main/java/com/provismet/CombatPlusCore/utility/CPCConfig.java
@@ -1,7 +1,26 @@
 package com.provismet.CombatPlusCore.utility;
 
-public abstract class CPCConfig {
-    public static final String FOLDER = "./config/combat-plus";
+import net.fabricmc.loader.api.FabricLoader;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public abstract class CPCConfig {
+    @Deprecated
+    public static final String FOLDER = "./config/combat-plus";
+    public static final Path FOLDER_PATH = FabricLoader.getInstance().getConfigDir().resolve("combat-plus");
     public static final String KEY_OVERRIDE_DATAPACK_LOOT_TABLES = "override_datapack_loot_tables";
+
+    public static Path getConfigDirectory () {
+        if (!Files.exists(FOLDER_PATH)) {
+            try {
+                Files.createDirectories(FOLDER_PATH);
+            } catch (IOException e) {
+                throw new RuntimeException("Creating Combat+ Core config directory", e);
+            }
+        }
+
+        return FOLDER_PATH;
+    }
 }
